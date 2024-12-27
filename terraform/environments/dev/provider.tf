b408a110c9678b538cc72e8d51ac7ef0d46892c7
jainrishabhk16 <jain.rishabhk16@gmail.com>
2024-12-27 19:46:33 +0530

    Update provider.tf

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -3,16 +3,12 @@ provider "aws" {
   region = var.aws_region
 }
 
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
-#   }
-# }
-
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+  }
 }
