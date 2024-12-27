b896a1cb061f3e925b3fbd48caaf58707a45e953
jainrishabhk16 <jain.rishabhk16@gmail.com>
2024-12-27 19:01:43 +0530

    add provider, locals, backend.tf

---
+++ terraform/environments/dev/backend.tf
@@ -0,0 +1,10 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "mybucket"
+#     key    = "path/to/my/key"
+#     region = "us-east-1"
+#   }
+# }
+
+
+# we can create the backend bucket here to store our terraform state file.
