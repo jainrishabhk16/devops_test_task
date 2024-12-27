ac693b4e4f64afeee76f6a496f1c30331ef4177d
jainrishabhk16 <jain.rishabhk16@gmail.com>
2024-12-27 19:52:55 +0530

    Create backend.tf

---
+++ terraform/environments/uat/backend.tf
@@ -0,0 +1,10 @@
+
+# we can create the backend bucket here to store our terraform state file.
+# # terraform {
+#   backend "s3" {
+#     bucket = "mybucket"
+#     key    = "path/to/my/key"
+#     region = "us-east-1"
+#   }
+# }
+
